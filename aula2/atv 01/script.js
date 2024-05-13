a32fd3094f4ed29ec32978bbb8d11a59c972d779
AkaiAsaro <162390772+AkaiAsaro@users.noreply.github.com>
2024-05-13 18:06:10 -0300

    aulinha

---
+++ aula2/atv 01/script.js
@@ -0,0 +1,18 @@
+// var a = "10"
+// var b = 10
+// if (a === b){
+//     console.log(`os valores são iguais`)
+// } else{
+//     console.log(`os valores não são iguais`)
+// }
+
+var number = parseFloat(prompt("digite um numero"))
+
+if (number > 0 && number % 2 == 0){
+    console.log(`${number} é positivo e par`)
+} else {
+    console.log(`${number} não é positivo e par`)
+}
+
+
+
